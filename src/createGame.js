c246185034b0c3ae7b595d7fab7f16707b0f50a6
A. Rzhanitsyn <risdeep@gmail.com>
2021-02-22 22:35:38 +0500

    add return if uncorrect answer

--- src/createGame.js
+++ src/createGame.js
@@ -20,6 +20,7 @@ export default (titleRulesOfTheGame, getQuestionAndAnswer) => () => {
     } else {
       console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${answer}'.`);
       console.log(`Let's try again, ${userName}!`);
+      return;
     }
   } while (countCorrectAnswer !== COUNT_CORRECT_ANSWER_TO_WIN);
 
